cf5252af60dc1a2236502f9f18db803cca847d43
Wiesława Bryzik <wieslawa.bryzik@gmail.com>
2020-03-22 20:13:44 +0100

    Add single Room & costs to data

--- src/components/views/Room/Room.js
+++ src/components/views/Room/Room.js
@@ -1,37 +1,84 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Grid, Row, Col } from 'react-flexbox-grid';
 
-import clsx from 'clsx';
+import PageTitle from '../../common/PageTitle/PageTitle';
 
-// import { connect } from 'react-redux';
-// import { reduxSelector, reduxActionCreator } from '../../../redux/exampleRedux.js';
+
+import { connect } from 'react-redux';
+import { getRoomById } from '../../../redux/roomsRedux.js';
+
 
 import styles from './Room.module.scss';
 
-const Component = ({className, children}) => (
-  <div className={clsx(className, styles.root)}>
-    <h2>Room</h2>
-    {children}
-  </div>
-);
-
-Component.propTypes = {
-  children: PropTypes.node,
-  className: PropTypes.string,
+const Component = ({ image, name, cost, person, difficulty, time, id, intro, description }) => {
+  return (
+    <section>
+      <Grid>
+        <PageTitle text={name} />
+        <Row>
+          <Col xs={12} sm={6} lg={2} className={styles.column}>
+            <article className={styles.component}>
+              <img src={image} alt={name} />
+              <h3 className={styles.title}>{name}</h3>
+              <div className={styles.details}>
+                <span>{person} osób</span>
+                <span>trudność: {difficulty}</span>
+                <span>{time} min</span>
+                <span> {cost}zł</span>
+              </div>
+            </article>
+          </Col>
+          <Col lg={10}>
+            <article className={styles.description}>
+              <h2>Co to za pokój?</h2>
+              <span>{intro}</span>
+              <h2>Co Cię tu spotka?</h2>
+              <span>{description}</span>
+            </article>
+          </Col>
+        </Row>
+      </Grid >
+      <Grid>
+        <Row>
+          <Col xs={12}>
+            <PageTitle text='Kup voucher' />
+            {/* <OrderForm tripCost={cost} tripName={name} tripId={id} countryCode={country.alpha3Code} /> */}
+          </Col>
+        </Row>
+      </Grid>
+    </section>
+  );
 };
 
-// const mapStateToProps = state => ({
-//   someProp: reduxSelector(state),
-// });
+Component.propTypes = {
+  image: PropTypes.string,
+  name: PropTypes.string,
+  person: PropTypes.string,
+  cost: PropTypes.string,
+  difficulty: PropTypes.string,
+  time: PropTypes.string,
+  id: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  intro: PropTypes.string,
+  description: PropTypes.string,
+};
+
+const mapStateToProps = (state, props) => {
+  const room = getRoomById(state, props.match.url.slice(6));
+
+  return {
+    ...room,
+  };
+};
 
 // const mapDispatchToProps = dispatch => ({
 //   someAction: arg => dispatch(reduxActionCreator(arg)),
 // });
 
-// const Container = connect(mapStateToProps, mapDispatchToProps)(Component);
+const Container = connect(mapStateToProps)(Component);
 
 export {
-  Component as Room,
-  // Container as Room,
+  // Component as Room,
+  Container as Room,
   Component as RoomComponent,
 };
